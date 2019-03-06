bec984b3d62a554b94eb222fad40a4922ece2e15
The-Kyle <oejava@gmail.com>
2019-03-05 17:05:05 -0800

    commit

--- js/typewriter.js
+++ js/typewriter.js
@@ -5,7 +5,7 @@ var aText = new Array(
     "Hello,", "my name is Kyle,", "a student web developer in Halifax."
   );
   
-  var iSpeed = 100; // time delay of print out
+  var iSpeed = 150; // time delay of print out
   var iIndex = 0; // start printing array at this posision
   var iArrLength = aText[0].length; // the length of the text array
   var iScrollAt = 20; // start scrolling up at this many lines
@@ -38,26 +38,7 @@ var aText = new Array(
     } else {
     setTimeout("typewriter()", iSpeed);
     }
-
-    console.log(iIndex)
-  
-    // fade in the rest of the elements on the page
-    // once the typewriter function is completed
-
-    // get elements and set opacity to 0
-    var homeBox = document.getElementById("home-box");
-    var webDevText = document.getElementById("web-dev-text");
-    var vancouverText = document.getElementById("vancouver-text");
-    var underline = document.getElementById("underline");
-
-    // when the typewriter ends, add class "transition"
-    if(iTextPos == 15){
-      homeBox.classList.add("transition");
-      webDevText.classList.add("transition");
-      vancouverText.classList.add("transition");
-      underline.classList.add("transition");
-    }
-  
+ 
   }
   
   typewriter();
